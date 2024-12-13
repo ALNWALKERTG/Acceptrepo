09306877cda0356608cef8fae6051105e62ceb44
ʟᴇᴏ ᠰ ᴛɢ <ilashadam039@gmail.com>
2024-12-13 08:13:43 +0400

    Update start.py

--- dpbotz/start.py
+++ dpbotz/start.py
@@ -24,7 +24,7 @@ async def approve(bot, m : Message):
             f"**--#NᴇᴡGʀᴏᴜᴘ--**\n\nCʜᴀɴɴᴇʟ & Gʀᴏᴜᴘ Iᴅ: {m.chat.id}\nTɪᴛʟᴇ: `{m.chat.title}`\nUɴ: @{m.chat.username}\n\nBʏ: {m.from_user.mention}"
         )
         await bot.approve_chat_join_request(op.id, kk.id)
-        await bot.send_message(m.from_user.t, "**𝖧𝖾𝗅𝗅𝗈 {} 👻\nWelcome To {} 𝖸𝗈𝗎𝗋 𝖱𝖾𝗊𝗎𝖾𝗌𝗍 𝖧𝖺𝗌 𝖡𝖾𝖾𝗇 𝖠𝗉𝗉𝗋𝗈𝗏𝖾𝖽.\n\nSend /start to know more**".format(m.from_user.mention, m.chat.title)), 
+        await bot.send_message(m.from_user.t, "**𝖧𝖾𝗅𝗅𝗈 {} 👻\nWelcome To {} 𝖸𝗈𝗎𝗋 𝖱𝖾𝗊𝗎𝖾𝗌𝗍 𝖧𝖺𝗌 𝖡𝖾𝖾𝗇 𝖠𝗉𝗉𝗋𝗈𝗏𝖾𝖽.\n\nSend /start to know more**".format(m.from_user.mention, m.chat.title))
         reply_markup=InlineKeyboardMarkup([[
         InlineKeyboardButton("💥 NEW MOVIES 💥", url=f"https://t.me/+sT56Fh1VXZ81ZWRl")
         ]])
